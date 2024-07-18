49726190361d8bf5738872e87aa6520d12a05c8c
mr-amanze <mr.amanzes4all@gmail.com>
2024-07-18 11:48:19 +0100

    Add files via upload

---
+++ webber/myscript.js
@@ -0,0 +1,19 @@
+window.alert("Hello!!! Confirm Login?")
+
+let date = new Date();
+document.write("Today's Date & Time is:  " + date + "<br/>")
+
+let result;
+let age = 20;
+if(age > 30) {
+    result = "Qaulified for the position";
+} else {
+    result = "Not qaulified for the position";
+}
+
+document.getElementsByClassName(sayhello).innerHTML
+function sayhello() {
+    alert("Hello there!");
+}
+
+ 
